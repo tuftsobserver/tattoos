a3ef50fe8c06857586531289b90eb607af295d41
mimmackk <mimmackattack@gmail.com>
2016-11-05 17:25:17 -0400

    first commit

---
+++ js/index_script.js
@@ -0,0 +1,16 @@
+function aud_play_pause(id) {
+  var audio = document.getElementById('audio' + id);
+  var player = document.getElementById('player' + id);
+  if (audio.paused) {
+  	// stop all other players
+  	for (var i = 1; i <= 12; ++i) {
+  		document.getElementById('audio' + i).pause();
+  		document.getElementById('player' + i).innerHTML = '';
+  	}
+    audio.play();
+    player.innerHTML = '<img src="img/audiobars.svg"/>';
+  } else {
+    audio.pause();
+    player.innerHTML = '';
+  }
+}
