38bc454c5135ed0ba6b8b6d8e79561a7ca559227
mimmackk <mimmackattack@gmail.com>
2016-11-06 12:06:21 -0500

    added photos and cleaned organizaton

--- js/index_script.js
+++ js/index_script.js
@@ -4,6 +4,9 @@ function aud_play_pause(id) {
   if (audio.paused) {
   	// stop all other players
   	for (var i = 1; i <= 12; ++i) {
+  		if (i == id) {
+  			continue;
+  		}
   		document.getElementById('audio' + i).pause();
   		document.getElementById('player' + i).innerHTML = '';
   	}
