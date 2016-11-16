e4ef40e539f3f3fcba26b2288e31edfb1477d444
mimmackk <mimmackattack@gmail.com>
2016-11-15 22:20:50 -0500

    added hover state

--- js/index_script.js
+++ js/index_script.js
@@ -1,19 +1,20 @@
 function aud_play_pause(id) {
-  var audio = document.getElementById('audio' + id);
-  var player = document.getElementById('player' + id);
-  if (audio.paused) {
-  	// stop all other players
-  	for (var i = 1; i <= 12; ++i) {
-  		if (i == id) {
-  			continue;
-  		}
-  		document.getElementById('audio' + i).pause();
-  		document.getElementById('player' + i).innerHTML = '';
-  	}
-    audio.play();
-    player.innerHTML = '<img src="img/audiobars.svg"/>';
-  } else {
-    audio.pause();
-    player.innerHTML = '';
-  }
+    var audio = document.getElementById('audio' + id);
+    var player = document.getElementById('player' + id);
+    if (audio.paused) {
+        // pause all other players
+        for (var i = 1; i <= 12; ++i) {
+            if (i == id) continue;
+            document.getElementById('audio' + i).pause();
+            document.getElementById('player' + i).innerHTML = '';
+            $('#grid-elem' + i).removeClass('active');
+        }
+        audio.play();
+        player.innerHTML = '<img src="img/audiobars.svg"/>';
+        $('#grid-elem' + id).addClass('active');
+    } else {
+        audio.pause();
+        player.innerHTML = '';
+        $('#grid-elem' + id).removeClass('active');
+    }
 }
